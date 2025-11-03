06369d4082ab5ef3b74e644b8cd877937bff996d
Kishore Kumar <ecekishore114@gmail.com>
2025-11-03 13:36:10 +0530

    Removed unnecessary files

--- README.md
+++
@@ -1,1 +0,0 @@
-# Kishoreramesh.github.io
--- main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".text", {
-	strings:["RPA developer","Youtuber","Electronic Project Developer"],
-	typeSpeed:70,
-	backSpeed:40,
-	backDelay:1000,
-	loop:true
-});
